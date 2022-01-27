db2ac6969aca5201421629ebd4297efe0aecee38
gyung <282203@naver.com>
2022-01-28 04:15:50 +0900

    커피숍 테스트

--- src/coffeeshoptest/Customer.java
+++ src/coffeeshoptest/Customer.java
@@ -18,7 +18,7 @@ public class Customer implements Icustomer {
     public void order (String menuName, Menu menu, Barista barista){
         MenuItem menuItem = menu.choose(menuName);
         Coffee coffee = barista.makeCoffee (menuItem);
-        System.out.println(name + "이(가) " + barista + "에게 " + menuItem.getPrice() + "원 " +
+        System.out.println(name + "이(가) " + menuItem.getPrice() + "원 " +
         menuItem.getName() + "를(을) 받았습니다.");
 
 
